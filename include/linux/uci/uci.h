8d93d097d3d7e90abfe5d06b54efdc19fb3571ce
tbalden <illespal@gmail.com>
2017-12-08 13:36:49 -0800

    uci: unprivileged configuration interface
    original idea and impl by @tbalden at xda
    
    Serve as unrooted, vulnerable configuration interface to parse
    settings from sdcard / data partition.
    Able to parse user config file on each screen off, or to poll system
    config file upon need in kernel. This can be useful to pass data
    to kernel from an unprivileged application/service in userspace
    writing to user filesystem.
    
    default locations /sdcard/uci_user.cfg /sdcard/uci_sys.cfg
    
    File format supported:
    key1=0
    ...
    keyN=222

---
+++ include/linux/uci/uci.h
@@ -0,0 +1,38 @@
+#ifndef __UCI_H__
+#define __UCI_H__
+
+#define UCI_INVALID_INT -999999
+
+#define UCI_USER_FILE "/storage/emulated/0/uci_user.cfg"
+#define UCI_SYS_FILE "/storage/emulated/0/uci_sys.cfg"
+#define UCI_USER_FILE_END "uci_user.cfg"
+#define UCI_SYS_FILE_END "uci_sys.cfg"
+
+extern bool is_uci_path(const char *file_name);
+extern bool is_uci_file(const char *file_name);
+
+extern void notify_uci_file_closed(const char *file_name);
+extern void notify_uci_file_write_opened(const char *file_name);
+
+/** accesing kernel settings from UCI property configuration
+*/
+extern int uci_get_user_property_int(const char* property, int default_value);
+extern int uci_get_user_property_int_mm(const char* property, int default_value, int min, int max);
+/** accesing kernel settings from UCI property configuration
+*/
+extern const char* uci_get_user_property_str(const char* property, const char* default_value);
+
+/** accesing sys variables from UCI sys props
+*/
+extern int uci_get_sys_property_int(const char* property, int default_value);
+extern int uci_get_sys_property_int_mm(const char* property, int default_value, int min, int max);
+/** accesing sys variables from UCI sys props
+*/
+extern const char* uci_get_sys_property_str(const char* property, const char* default_value);
+
+/** add change listener to sys cfg*/
+extern void uci_add_sys_listener(void (*f)(void));
+/** add change listener to user cfg*/
+extern void uci_add_user_listener(void (*f)(void));
+
+#endif /* __UCI_H__ */
